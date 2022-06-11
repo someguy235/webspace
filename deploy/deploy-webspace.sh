6906afd9de237ebd443d977eb34e4e1ea34bbbd2
Ethan Shepherd <ethan.m.shepherd@gmail.com>
2022-06-11 15:01:10 -0700

    deploy script

---
+++ deploy/deploy-webspace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo ""
+
+echo "move to deploy folder..."
+cd /var/www/webspace
+
+echo "updating from github..."
+git pull
+
+echo "installing dependencies..."
+npm ci
+
+echo "cleaning dist directory..."
+rm -r dist
+
+echo "building app..."
+npm run build
+
+echo "copy static resources..."
+cp ./resources/files/* ./dist
+
+echo "restarting server..."
+sudo systemctl reload nginx
