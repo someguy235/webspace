ba68b9eb06f9920b425add69b7b0572d02ba2c45
Ethan Shepherd <ethan.m.shepherd@gmail.com>
2022-06-21 17:20:33 -0700

    add logos for project page

--- src/js/Projects.js
+++ src/js/Projects.js
@@ -1,5 +1,22 @@
 import projects from "../data/projects.json";
 
+import android_logo from "../img/logos/android.png";
+import angularjs_logo from "../img/logos/angularjs.png";
+import d3_logo from "../img/logos/d3.png";
+import express_logo from "../img/logos/express.png";
+import grails_logo from "../img/logos/grails.png";
+import groovy_logo from "../img/logos/groovy.png";
+import jquery_logo from "../img/logos/jquery.png";
+import js_logo from "../img/logos/javascript.png";
+import leaflet_logo from "../img/logos/leaflet.png";
+import mongo_logo from "../img/logos/mongo.png";
+import node_logo from "../img/logos/node.png";
+import opencv_logo from "../img/logos/opencv.png";
+import postgres_logo from "../img/logos/postgres.png";
+import python_logo from "../img/logos/python.png";
+import react_logo from "../img/logos/react.png";
+import ts_logo from "../img/logos/typescript.png";
+
 const ProjectTitle = ({ project }) => {
   if (!project.name) return null;
   return (
@@ -26,9 +43,37 @@ const ProjectTitle = ({ project }) => {
 
 const ProjectTech = ({ project }) => {
   if (!project.tech || project.tech.length === 0) return null;
+  const logoMap = {
+    Android: android_logo,
+    AngularJS: angularjs_logo,
+    D3JS: d3_logo,
+    Express: express_logo,
+    Grails: grails_logo,
+    Groovy: groovy_logo,
+    JavaScript: js_logo,
+    jQuery: jquery_logo,
+    LeafletJS: leaflet_logo,
+    MongoDB: mongo_logo,
+    NodeJS: node_logo,
+    OpenCV: opencv_logo,
+    Postgres: postgres_logo,
+    Python: python_logo,
+    ReactJS: react_logo,
+    TypeScript: ts_logo,
+  };
   return (
     <div className="project-tech">
-      {project.tech.map((tech) => tech).join(" - ")}
+      {project.tech.map((tech) => {
+        if (logoMap[tech]) {
+          return (
+            <span key={tech}>
+              <img src={logoMap[tech]} alt={tech} title={tech} />
+            </span>
+          );
+        } else {
+          return <span key={tech}>{tech}</span>;
+        }
+      })}
     </div>
   );
 };
