cfd247b76b3bd8e532cd519cee0b6a7ba7282991
Ethan Shepherd <ethan.m.shepherd@gmail.com>
2026-08-08 17:14:10 -0700

    fix deploy script, update dependencies

--- deploy-scripts/deploy-webspace.sh
+++ deploy-scripts/deploy-webspace.sh
@@ -2,6 +2,15 @@
 
 echo ""
 
+echo "sourcing .bashrc for nvm..."
+# Load profile so NVM and its PATH additions are available in non-login shells
+export HOME="${HOME:-$(eval echo ~$(whoami))}"
+[ -f "$HOME/.bash_profile" ] && source "$HOME/.bash_profile"
+[ -f "$HOME/.bashrc" ] && source "$HOME/.bashrc"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 echo "move to deploy folder..."
 cd /var/www/webspace
 
